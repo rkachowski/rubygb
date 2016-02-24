aa358d5a7d21ff7c43f9bdc026a593d99cbfe6cc
Donald Hutchison <git@toastymofo.net>
2016-02-24 21:12:19 +0100

    Add a no fix option so i can screw up the logo all i want.

--- lib/rubygb/cli.rb
+++ lib/rubygb/cli.rb
@@ -3,8 +3,9 @@ require 'thor'
 module Rubygb
   class CLI < Thor
     desc 'build FILENAME', 'attempt to assemble, link and fix FILENAME and create a gb rom from it'
+    option :no_fix, :type => :boolean
     def build filename
-      Rubygb.build filename
+      Rubygb.build filename, options
     end
   end
 end
--- lib/rubygb/rubygb.rb
+++ lib/rubygb/rubygb.rb
@@ -1,5 +1,5 @@
 module Rubygb
-  def self.build filename
+  def self.build filename, options={}
     unless File.exists? filename
       raise "Fatal: can't find #{filename}"
     end
@@ -9,7 +9,7 @@ module Rubygb
     exe_path = File.expand_path(File.join(File.dirname(__FILE__),"..","rgbds"))
     puts `#{exe_path}/rgbasm -v -o#{base}.obj #{filename}`
     puts `#{exe_path}/rgblink -m#{base}.map -n#{base}.sym -o#{base}.gb #{base}.obj`
-    puts `#{exe_path}/rgbfix -p0 -v #{base}.gb`
+    puts `#{exe_path}/rgbfix -p0 -v #{base}.gb` unless options[:no_fix]
   end
 end
 
