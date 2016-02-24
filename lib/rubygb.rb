9486ce4bd120d60a2ca9f3b9fcad31025bdf2d6b
Donald Hutchison <git@toastymofo.net>
2016-02-24 20:24:44 +0100

    Fix cli.

--- lib/rubygb.rb
+++ lib/rubygb.rb
@@ -1,4 +1,6 @@
 require_relative "rubygb/version"
+require_relative "rubygb/rubygb"
+require_relative "rubygb/cli"
 
 module Rubygb
 end
--- lib/rubygb/cli.rb
+++ lib/rubygb/cli.rb
@@ -1,5 +1,8 @@
+require 'thor'
+
 module Rubygb
   class CLI < Thor
+    desc 'build FILENAME', 'attempt to assemble, link and fix FILENAME and create a gb rom from it'
     def build filename
       Rubygb.build filename
     end
