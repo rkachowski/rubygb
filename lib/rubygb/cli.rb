223e6f5fa725c9ff845e1c6cfce07aa1651a9e7f
Donald Hutchison <git@toastymofo.net>
2016-02-26 22:33:19 +0100

    Adding galp files.

--- lib/rubygb/cli.rb
+++ lib/rubygb/cli.rb
@@ -2,7 +2,7 @@ require 'thor'
 
 module Rubygb
   class CLI < Thor
-    desc 'build FILENAME', 'attempt to assemble, link and fix FILENAME and create a gb rom from it'
+    desc 'build FILENAME', 'attempt to assemble, link + fix FILENAME and create a gb rom from it'
     option :no_fix, :type => :boolean
     def build filename
       Rubygb.build filename, options
